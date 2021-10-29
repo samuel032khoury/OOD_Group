ab455f3f9897c396cd0241c77cab3b94d08c44ed
samuel032khoury <Ji.xian@northeastern.edu>
2021-10-29 13:49:14 -0400

    update flip

--- HW04/src/model/ImageProcessModel.java
+++ HW04/src/model/ImageProcessModel.java
@@ -1,14 +1,14 @@
 package model;
 
-import java.awt.image.BufferedImage;
+import java.awt.Color;
 import java.io.File;
 
 public interface ImageProcessModel {
-  public void load(String imageName, BufferedImage image);
+  public void load(String imageName, Color[][] image);
   public void vertiFlip(String imageName);
   public void horizFlip(String imageName);
   public void brighten(String imageName);
   public void darken(String imageName);
   public void greyscale(String imageName);
-  public BufferedImage getImage(String imageName) throws IllegalArgumentException;
+  public Color[][] getImage(String imageName) throws IllegalArgumentException;
 }
