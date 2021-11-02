dc626aa99006f30b9fdd7e6754759b6fd0674ede
Eric-szh <eric.szh@hotmail.com>
2021-11-02 02:14:40 -0400

    Started tests

---
+++ HW04/test/controller/utils/LoadManagerTest.java
@@ -0,0 +1,12 @@
+package controller.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoadManagerTest {
+  @Test
+  public void testLoad() throws Exception {
+
+  }
+}
