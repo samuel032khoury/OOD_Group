9b138e0e226803527ddef092a2ffea73f6ba96eb
samuel032khoury <Ji.xian@northeastern.edu>
2021-11-03 14:22:15 -0400

    update controller impl javadoc

--- HW04/src/controller/controller/ImageProcessControllerImpl.java
+++ HW04/src/controller/controller/ImageProcessControllerImpl.java
@@ -24,26 +24,58 @@ import model.operation.SingleChannelOperator;
 import view.IImageProcessView;
 import view.SimpleImageProcessViewImpl;
 
-// TODO
+/**
+ * To represent an image process controller that has a command set to deal with user inputs, and
+ * update the model according to the valid command.
+ */
 public class ImageProcessControllerImpl implements IImageProcessController {
   private final ImageLibModel model;
   private final Readable input;
   private final IImageProcessView view;
   private final Map<String, Supplier<ICommand>> cmdMap;
 
+  /**
+   * A convenient constructor for building an image process controller with a default {@link
+   * ImageLibModelImpl}.
+   */
   public ImageProcessControllerImpl() {
     this(new ImageLibModelImpl());
   }
 
-  public ImageProcessControllerImpl(ImageLibModel model) {
+  /**
+   * An {@link ImageLibModelImpl}-customizable constructor for building an image process
+   * controller.
+   *
+   * @param model the customized library model for controller to use
+   * @throws IllegalArgumentException when the provided {@code model} is null
+   */
+  public ImageProcessControllerImpl(ImageLibModel model) throws IllegalArgumentException {
     this(model, new InputStreamReader(System.in));
   }
 
-  public ImageProcessControllerImpl(ImageLibModel model, Readable input) {
+  /**
+   * An {@link ImageLibModelImpl}-customizable constructor for building an image process controller,
+   * with a specified {@link Readable} object.
+   *
+   * @param model the customized library model for controller to use
+   * @param input a specified {@link Readable} object
+   * @throws IllegalArgumentException when the provided {@code model} or {@code input} is null
+   */
+  public ImageProcessControllerImpl(ImageLibModel model, Readable input)
+          throws IllegalArgumentException {
     this(model, input, new SimpleImageProcessViewImpl(model));
   }
 
-  public ImageProcessControllerImpl(ImageLibModel model, Readable input, IImageProcessView view) {
+  /**
+   * A fully customizable constructor for building an image process controller.
+   *
+   * @param model the customized library model for controller to use
+   * @param input a specified {@link Readable} object
+   * @param view a target {@link IImageProcessView} for view purpose
+   * @throws IllegalArgumentException when any of the provided arguments is null
+   */
+  public ImageProcessControllerImpl(ImageLibModel model, Readable input, IImageProcessView view)
+          throws IllegalArgumentException {
     if (model == null || input == null || view == null) {
       throw new IllegalArgumentException("Illegal inputs for generating a new image process "
               + "controller");
@@ -52,24 +84,31 @@ public class ImageProcessControllerImpl implements IImageProcessController {
     this.input = input;
     this.view = view;
     this.cmdMap = new HashMap<>() {{
-        put("vertical-flip", () -> new FlipCommand(true));
-        put("horizontal-flip", () -> new FlipCommand(false));
-        put("brighten", () -> new AdjustBrightnessCommand(true));
-        put("darken", () -> new AdjustBrightnessCommand(false));
-        put("blue-component", () -> new GreyCommand(SingleChannelOperator.Blue));
-        put("red-component", () -> new GreyCommand(SingleChannelOperator.Red));
-        put("green-component", () -> new GreyCommand(SingleChannelOperator.Green));
-        put("luma-component", () -> new GreyCommand(SimpleArithmeticChannelOperator.Luma));
-        put("value-component", () -> new GreyCommand(SimpleArithmeticChannelOperator.Value));
-        put("intensity-component",
-            () -> new GreyCommand(SimpleArithmeticChannelOperator.Intensity));
-        put("load", LoadCommand::new);
-        put("save", SaveCommand::new);
-        put("size", SizeCommand::new);
-        put("QUIT", QuitCommand::new);
-      }};
+      put("vertical-flip", () -> new FlipCommand(true));
+      put("horizontal-flip", () -> new FlipCommand(false));
+      put("brighten", () -> new AdjustBrightnessCommand(true));
+      put("darken", () -> new AdjustBrightnessCommand(false));
+      put("blue-component", () -> new GreyCommand(SingleChannelOperator.Blue));
+      put("red-component", () -> new GreyCommand(SingleChannelOperator.Red));
+      put("green-component", () -> new GreyCommand(SingleChannelOperator.Green));
+      put("luma-component", () -> new GreyCommand(SimpleArithmeticChannelOperator.Luma));
+      put("value-component", () -> new GreyCommand(SimpleArithmeticChannelOperator.Value));
+      put("intensity-component",
+              () -> new GreyCommand(SimpleArithmeticChannelOperator.Intensity));
+      put("load", LoadCommand::new);
+      put("save", SaveCommand::new);
+      put("size", SizeCommand::new);
+      put("QUIT", QuitCommand::new);
+    }};
   }
 
+  /**
+   * This method runs the program, expecting user's input from the {@link Readable}, updates the
+   * {@link ImageLibModel} model and let view print feedback message according to the received
+   * valid command. Whenever the command is not valid (unsupported behavior/ unmatched arguments
+   * for a supported command), it tells view to print informative error message and let user try
+   * again.
+   */
   @Override
   public void run() {
     Scanner scanner = new Scanner(input);
