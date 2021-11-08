02370aa7591b972e64501cc754ff14a62640c123
samuel032khoury <Ji.xian@northeastern.edu>
2021-11-08 14:09:07 -0500

    return tests

---
+++ HW05/test/utils/MockChannelOperator.java
@@ -0,0 +1,10 @@
+package utils;
+
+import model.operation.IChannelOperator;
+
+/**
+ * A MockChannelOperator to introduce error in the test.
+ */
+public enum MockChannelOperator implements IChannelOperator {
+  mock;
+}
