9b77a4d3f7f5b9e13a21e4246d6b2c6dbfa15d98
samuel032khoury <Ji.xian@northeastern.edu>
2021-11-10 18:09:50 -0500

    format to upper cases

--- HW05/src/controller/utils/UniWriter.java
+++ HW05/src/controller/utils/UniWriter.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Objects;
 
 import javax.imageio.ImageIO;
@@ -76,17 +77,12 @@ public class UniWriter implements IWriter {
 
     String[] list = fileName.split("\\.");
 
-    String format = list[list.length - 1];
-
-    if (Objects.equals(format, "bmp")) {
-      format = "BMP";
-    }
+    String format = list[list.length - 1].toUpperCase();
 
     try {
       ImageIO.write(image, format, file);
     } catch (IOException e) {
       throw new IllegalStateException("An error occurs when trying to save the file!");
     }
-
   }
 }
