3aa5db49bd9a223cc707fb5ba1274aa77201f2e3
samuel032khoury <Ji.xian@northeastern.edu>
2021-11-10 16:55:33 -0500

    update model testing

--- HW05/test/ModelTest.java
+++ HW05/test/ModelTest.java
@@ -1,81 +1,90 @@
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertTrue;
-//
-//import java.awt.Color;
-//
-//import model.imagefile.ImageFile;
-//import model.imagefile.ImageFileImpl;
-//import model.imagefile.ReadOnlyImageFile;
-//import model.operation.SimpleArithmeticChannelOperator;
-//import model.operation.SingleChannelOperator;
-//import utils.MockChannelOperator;
-//
-///**
-// * To test the model for the project, including {@link ImageFile} model
-// * and {@link model.library.ImageLibModel} model.
-// */
-//public class ModelTest {
-//  Color c1;
-//  Color c2;
-//  Color c3;
-//  Color c6;
-//  Color c4;
-//  Color c5;
-//  Color c7;
-//  Color c8;
-//  Color c9;
-//  Color[] loc1;
-//  Color[] loc2;
-//  Color[] loc3;
-//  Color[][] imgL;
-//  ImageFile imgF;
-//
-//  @Before
-//  public void setUp() {
-//    c1 = new Color(0, 0, 0);
-//    c2 = new Color(100, 100, 100);
-//    c3 = new Color(200, 200, 200);
-//    c6 = new Color(0, 50, 100);
-//    c4 = new Color(50, 100, 150);
-//    c5 = new Color(150, 200, 250);
-//    c7 = new Color(123, 45, 67);
-//    c8 = new Color(234, 56, 78);
-//    c9 = new Color(134, 156, 178);
-//    loc1 = new Color[]{c1, c2, c3};
-//    loc2 = new Color[]{c4, c5, c6};
-//    loc3 = new Color[]{c7, c8, c9};
-//    imgL = new Color[][]{loc1, loc2, loc3};
-//    imgF = new ImageFileImpl(imgL);
-//  }
-//
-//  @Test(expected = IllegalArgumentException.class)
-//  public void TestConstructExceptionNullImage() {
-//    new ImageFileImpl(null);
-//  }
-//
-//  @Test(expected = IllegalArgumentException.class)
-//  public void TestConstructExceptionNegMaxVal() {
-//    new ImageFileImpl(this.imgL, -100);
-//  }
-//
-//  @Test(expected = IllegalArgumentException.class)
-//  public void TestConstructExceptionNullColorRow() {
-//    Color[][] imgLAlt = new Color[][]{this.loc1, this.loc2, null};
-//    new ImageFileImpl(imgLAlt);
-//  }
-//
-//  @Test(expected = IllegalArgumentException.class)
-//  public void TestConstructExceptionNullColorEle() {
-//    this.loc3 = new Color[]{c7, null, c9};
-//    Color[][] imgLAlt = new Color[][]{this.loc1, this.loc2, this.loc3};
-//    new ImageFileImpl(imgLAlt);
-//  }
-//
-//  @Test
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Color;
+
+import model.imagefile.ImageFile;
+import model.imagefile.ImageFileImpl;
+import model.imagefile.ReadOnlyImageFile;
+import utils.MockChannelOperator;
+
+/**
+ * To test the model for the project, including {@link ImageFile} model and {@link
+ * model.library.ImageLibModel} model.
+ */
+public class ModelTest {
+  Color c1;
+  Color c2;
+  Color c3;
+  Color c6;
+  Color c4;
+  Color c5;
+  Color c7;
+  Color c8;
+  Color c9;
+  Color[] loc1;
+  Color[] loc2;
+  Color[] loc3;
+  Color[][] imgL;
+  ImageFile imgF;
+
+  @Before
+  public void setUp() {
+    c1 = new Color(0, 0, 0);
+    c2 = new Color(100, 100, 100);
+    c3 = new Color(200, 200, 200);
+    c6 = new Color(0, 50, 100);
+    c4 = new Color(50, 100, 150);
+    c5 = new Color(150, 200, 250);
+    c7 = new Color(123, 45, 67);
+    c8 = new Color(234, 56, 78);
+    c9 = new Color(134, 156, 178);
+    loc1 = new Color[]{c1, c2, c3};
+    loc2 = new Color[]{c4, c5, c6};
+    loc3 = new Color[]{c7, c8, c9};
+    imgL = new Color[][]{loc1, loc2, loc3};
+    imgF = new ImageFileImpl(imgL, 255, false);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void TestConstructExceptionNullImage() {
+    new ImageFileImpl(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void TestConstructExceptionNegMaxVal() {
+    new ImageFileImpl(this.imgL, -100);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void TestConstructExceptionNullColorRow() {
+    Color[][] imgLAlt = new Color[][]{this.loc1, this.loc2, null};
+    new ImageFileImpl(imgLAlt);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void TestConstructExceptionNullColorEle() {
+    this.loc3 = new Color[]{c7, null, c9};
+    Color[][] imgLAlt = new Color[][]{this.loc1, this.loc2, this.loc3};
+    new ImageFileImpl(imgLAlt);
+  }
+
+  @Test
+  public void TestCopyImage() {
+    ImageFile actualCopy = this.imgF.copy();
+    assertFalse(this.imgF == actualCopy);
+    assertFalse(sameContents(this.imgL, actualCopy, true));
+    assertTrue(sameContents(this.imgL, actualCopy, false));
+    assertEquals(imgF.getMaxColorVal(), actualCopy.getMaxColorVal());
+    assertEquals(imgF.alpha(), actualCopy.alpha());
+  }
+
+  //  @Test
 //  public void TestVertiFlip() {
 //    Color[][] expected = new Color[][]{this.loc3, this.loc2, this.loc1};
 //    ReadOnlyImageFile actual = this.imgF.vertiFlip();
@@ -234,59 +243,63 @@
 //    imgF.greyscale(MockChannelOperator.mock);
 //  }
 //
-//  @Test
-//  public void TestGetHeight() {
-//    assertEquals(3, imgF.getHeight());
-//    Color[][] imgLAlt = new Color[][]{loc1, loc2, loc3, loc1, loc2, loc3};
-//    ReadOnlyImageFile imgFAlt = new ImageFileImpl(imgLAlt);
-//    assertEquals(6, imgFAlt.getHeight());
-//  }
-//
-//  @Test
-//  public void TestGetWidth() {
-//    assertEquals(3, imgF.getWidth());
-//    Color[] loc1DoubleLength = new Color[]{c1, c2, c3, c1, c2, c3};
-//    Color[] loc2DoubleLength = new Color[]{c4, c5, c6, c4, c5, c6};
-//    Color[] loc3DoubleLength = new Color[]{c7, c8, c9, c7, c8, c9};
-//    Color[][] imgLAlt = new Color[][]{loc1DoubleLength, loc2DoubleLength, loc3DoubleLength};
-//    ReadOnlyImageFile imgFAlt = new ImageFileImpl(imgLAlt);
-//    assertEquals(6, imgFAlt.getWidth());
-//  }
-//
-//  @Test
-//  public void TestAlpha() {
-//    ReadOnlyImageFile imgFAlt = new ImageFileImpl(this.imgL, 20);
-//    assertFalse(imgF.alpha());
-//    assertFalse(imgFAlt.alpha());
-//  }
-//
-//  @Test
-//  public void TestGetMaxColorVal() {
-//    assertEquals(255, imgF.getMaxColorVal());
-//    ReadOnlyImageFile imgFAlt = new ImageFileImpl(this.imgL, 20);
-//    assertEquals(20, imgFAlt.getMaxColorVal());
-//  }
-//
-//  @Test
-//  public void TestGetColorAt() {
-//    assertEquals(c1, imgF.getColorAt(0, 0));
-//    assertEquals(c2, imgF.getColorAt(0, 1));
-//    assertEquals(c3, imgF.getColorAt(0, 2));
-//    assertEquals(c4, imgF.getColorAt(1, 0));
-//    assertEquals(c5, imgF.getColorAt(1, 1));
-//    assertEquals(c6, imgF.getColorAt(1, 2));
-//    assertEquals(c7, imgF.getColorAt(2, 0));
-//    assertEquals(c8, imgF.getColorAt(2, 1));
-//    assertEquals(c9, imgF.getColorAt(2, 2));
-//  }
-//
-//  private boolean sameContents(Color[][] imgL, ReadOnlyImageFile imgF) {
-//    boolean same = true;
-//    for (int i = 0; i < imgF.getHeight(); i++) {
-//      for (int j = 0; j < imgF.getWidth(); j++) {
-//        same = same & imgF.getColorAt(i, j).equals(imgL[i][j]);
-//      }
-//    }
-//    return same;
-//  }
-//}
+  @Test
+  public void TestGetHeight() {
+    assertEquals(3, imgF.getHeight());
+    Color[][] imgLAlt = new Color[][]{loc1, loc2, loc3, loc1, loc2, loc3};
+    ReadOnlyImageFile imgFAlt = new ImageFileImpl(imgLAlt);
+    assertEquals(6, imgFAlt.getHeight());
+  }
+
+  @Test
+  public void TestGetWidth() {
+    assertEquals(3, imgF.getWidth());
+    Color[] loc1DoubleLength = new Color[]{c1, c2, c3, c1, c2, c3};
+    Color[] loc2DoubleLength = new Color[]{c4, c5, c6, c4, c5, c6};
+    Color[] loc3DoubleLength = new Color[]{c7, c8, c9, c7, c8, c9};
+    Color[][] imgLAlt = new Color[][]{loc1DoubleLength, loc2DoubleLength, loc3DoubleLength};
+    ReadOnlyImageFile imgFAlt = new ImageFileImpl(imgLAlt);
+    assertEquals(6, imgFAlt.getWidth());
+  }
+
+  @Test
+  public void TestAlpha() {
+    ReadOnlyImageFile imgFAlt = new ImageFileImpl(this.imgL, 20);
+    assertFalse(imgF.alpha());
+    assertFalse(imgFAlt.alpha());
+  }
+
+  @Test
+  public void TestGetMaxColorVal() {
+    assertEquals(255, imgF.getMaxColorVal());
+    ReadOnlyImageFile imgFAlt = new ImageFileImpl(this.imgL, 20);
+    assertEquals(20, imgFAlt.getMaxColorVal());
+  }
+
+  @Test
+  public void TestGetColorAt() {
+    assertEquals(c1, imgF.getColorAt(0, 0));
+    assertEquals(c2, imgF.getColorAt(0, 1));
+    assertEquals(c3, imgF.getColorAt(0, 2));
+    assertEquals(c4, imgF.getColorAt(1, 0));
+    assertEquals(c5, imgF.getColorAt(1, 1));
+    assertEquals(c6, imgF.getColorAt(1, 2));
+    assertEquals(c7, imgF.getColorAt(2, 0));
+    assertEquals(c8, imgF.getColorAt(2, 1));
+    assertEquals(c9, imgF.getColorAt(2, 2));
+  }
+
+  private boolean sameContents(Color[][] imgL, ReadOnlyImageFile imgF, boolean refEqul) {
+    boolean same = true;
+    for (int i = 0; i < imgF.getHeight(); i++) {
+      for (int j = 0; j < imgF.getWidth(); j++) {
+        if (refEqul) {
+          same = same & imgF.getColorAt(i, j) == (imgL[i][j]);
+        } else {
+          same = same & imgF.getColorAt(i, j).equals(imgL[i][j]);
+        }
+      }
+    }
+    return same;
+  }
+}
