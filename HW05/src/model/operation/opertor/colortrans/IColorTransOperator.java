60edc128faefeb7f6c96bedd6780d76a6473957a
Eric-szh <eric.szh@hotmail.com>
2021-11-10 14:53:33 -0500

    part javadoc

--- HW05/src/model/operation/opertor/colortrans/IColorTransOperator.java
+++ HW05/src/model/operation/opertor/colortrans/IColorTransOperator.java
@@ -5,6 +5,9 @@ import model.operation.function.IColorTransformFunction;
 /**
  * Interfaces for enums, each enum member should be the name of a channel operation and has its
  * corresponding {@link IColorTransformFunction} implemented.
+ *
+ * Each enum contains a matrix, the matrix contains the information about how to transfrom a color
+ * pixels.
  */
 public interface IColorTransOperator {
   double[][] getMatrix();
