610385f20cdfc3f37ea8ed88cd8a015486ea705d
Eric-szh <eric.szh@hotmail.com>
2021-11-20 23:43:42 -0500

    finished max color scale, changed the color for intensity

--- HW06/src/view/GUIIViewImpl.java
+++ HW06/src/view/GUIIViewImpl.java
@@ -358,20 +358,51 @@ public class GUIIViewImpl extends JFrame implements IGUIIView, ActionListener, L
     this.drawHistogram(this.histogramGraph, this.histogram);
   }
 
+  private int getMaxPixel(List<List<Integer>> histogram) {
+    int maxR = 0;
+    int maxG = 0;
+    int maxB = 0;
+    int maxI = 0;
+    List<Integer> redList = histogram.get(0);
+    List<Integer> greenList = histogram.get(1);
+    List<Integer> blueList = histogram.get(2);
+    List<Integer> intensityList = histogram.get(3);
+
+    for (int i = 0; i < histogram.get(0).size(); i++) {
+      if (redList.get(i) > maxR) {
+        maxR = redList.get(i);
+      }
+
+      if (greenList.get(i) > maxG) {
+        maxG = greenList.get(i);
+      }
+
+      if (blueList.get(i) > maxB) {
+        maxB = blueList.get(i);
+      }
+
+      if (intensityList.get(i) > maxI) {
+        maxI = intensityList.get(i);
+      }
+    }
+
+    return Math.max(Math.max(maxB, maxG), Math.max(maxI, maxR));
+  }
+
   private void drawHistogram(JPanel panel, List<List<Integer>> histogram) {
     int width = panel.getWidth();
     int height = panel.getHeight();
-    int pixels = width * height;
+    int maxPixels =this.getMaxPixel(histogram);
     Graphics g = panel.getGraphics();
     g.clearRect(0,0,width,height);
-    int verticalOffset = 3;
-    int horizontalOffset = 4;
+    int verticalOffset = 10;
+    int horizontalOffset = 10;
     double xSeparation = (width - 2.0 * horizontalOffset) / ( histogram.get(0).size() - 1.0 );
-    double ySeparation = (height - 2.0 * verticalOffset) / pixels ;
+    double ySeparation = (height - 2.0 * verticalOffset) / maxPixels ;
 
     int[] pX = new int[256];
     for (int i = 0; i < histogram.get(0).size(); i ++) {
-      pX[i] = (int) (verticalOffset + i * xSeparation);
+      pX[i] = (int) (horizontalOffset + i * xSeparation);
     }
     int[] pYR = new int[256];
     int[] pYG = new int[256];
@@ -383,10 +414,10 @@ public class GUIIViewImpl extends JFrame implements IGUIIView, ActionListener, L
       int numOfPixelB = histogram.get(2).get(i);
       int numOfPixelI = histogram.get(3).get(i);
       
-      pYR[i] = (int) (verticalOffset + (pixels - numOfPixelR) * ySeparation);
-      pYG[i] = (int) (verticalOffset + (pixels - numOfPixelG) * ySeparation);
-      pYB[i] = (int) (verticalOffset + (pixels - numOfPixelB) * ySeparation);
-      pYI[i] = (int) (verticalOffset + (pixels - numOfPixelI) * ySeparation);
+      pYR[i] = (int) (verticalOffset + (maxPixels - numOfPixelR) * ySeparation);
+      pYG[i] = (int) (verticalOffset + (maxPixels - numOfPixelG) * ySeparation);
+      pYB[i] = (int) (verticalOffset + (maxPixels - numOfPixelB) * ySeparation);
+      pYI[i] = (int) (verticalOffset + (maxPixels - numOfPixelI) * ySeparation);
     }
     g.setColor(new Color(255,0,0, 125));
     g.drawPolyline(pX, pYR, pX.length);
@@ -394,7 +425,7 @@ public class GUIIViewImpl extends JFrame implements IGUIIView, ActionListener, L
     g.drawPolyline(pX, pYG, pX.length);
     g.setColor(new Color(0,0,255, 125));
     g.drawPolyline(pX, pYB, pX.length);
-    g.setColor(new Color(125,125,125, 125));
+    g.setColor(new Color(0,0,0, 200));
     g.drawPolyline(pX, pYI, pX.length);
   }
 
